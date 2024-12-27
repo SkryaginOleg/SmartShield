7ecd2bf43c7854bf38a35c4132932ad1af5dc87c
SkryaginOleg <olegslav09@gmail.com>
2024-12-27 19:10:15 +0200

    SmartShield-3

--- SmartShield/urls.py
+++ SmartShield/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.shortcuts import redirect
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -44,4 +45,6 @@ urlpatterns = [
     path('api/v1/admin/', include('adminapp.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    path('', lambda request: redirect('/swagger/')),
 ]
